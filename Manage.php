63cfded56ec75a530321ac6cacf197b1fa2ebb52
Axodh <37446105+Axodh@users.noreply.github.com>
2019-02-23 15:51:50 +0100

    Modal Modif

--- Manage.php
+++ Manage.php
@@ -16,12 +16,29 @@ class Manage{
        <td>'.$member->pseudo.'</td>
        <td>'.$member->age.'</td>
        <td>'.$member->gender.'</td>
-       <td>
-       <form method="GET" action="$this->editUser($result)">
-         <button type="submit" class="btn btn-primary">
-         </button>
-        </form>
-        </td>
+       <td><button onclick="openModal('.$member->id.')" id="modalBtn" class="btn btn-primary"></button>
+          <div class="modal" id="'.$member->id.'">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Modify User Info</h5> <button onclick="closeModal('.$member->id.')" type="button" class="closeBtn btn btn-danger" data-dismiss="modal"> <span>x</span> </button>
+                </div>
+                <div class="modal-body">
+                      <p>Mail</p>
+                      <input name="mail" type="text" value="'.$member->mail.'"/>
+                      <p>Pseudo</p>
+                      <input name="pseudo" type="text" value="'.$member->pseudo.'"/>
+                      <p>Age</p>
+                      <input name="age" type="text" value="'.$member->age.'"/>
+                      <p>Gender</p>
+                      <input name="gender" type="text" value="'.$member->gender.'"/>
+                </div>
+                <div class="modal-footer"> <button type="button" class="btn btn-primary">Save changes</button> <button type="button" onclick="closeModal('.$member->id.')" class="btn btn-secondary" data-dismiss="modal">Close</button> </div>
+              </div>
+            </div>
+          </div>
+
+       </td>
      </tr>
      ';
    endforeach;
--- backOffice.php
+++ backOffice.php
@@ -4,7 +4,7 @@ require_once "Database.php";
 require_once "Manage.php";
 
 $db = new Database("viaxe","localhost","root","");
-$result = $db->query("SELECT mail,pseudo,age,gender FROM customer");
+$result = $db->query("SELECT id,mail,pseudo,age,gender FROM customer");
 
 
 
@@ -54,13 +54,16 @@ $result = $db->query("SELECT mail,pseudo,age,gender FROM customer");
               </thead>
               <tbody>
                 <?php $manage = new Manage($result);  ?>
+
               </tbody>
             </table>
+
           </div>
         </div>
       </div>
     </div>
   </div>
+  <script src="modal.js"> </script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
