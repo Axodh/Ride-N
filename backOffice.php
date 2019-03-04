7510087f9e66b41bd7c11639494ff2bf9bb6e30e
Axodh <37446105+Axodh@users.noreply.github.com>
2019-03-04 15:27:08 +0100

    Navbars

--- backOffice.php
+++ backOffice.php
@@ -3,7 +3,7 @@ session_start();
 ini_set("display_errors", 1);
 require_once "Database.php";
 require_once "Manage.php";
-
+require_once "navbar.php";
 $db = new Database("viaxe","localhost","root","");
 $resultUnbanned = $db->query("SELECT id,mail,pseudo,age,gender,isBanned FROM customer WHERE isBanned = 0");
 $resultBanned = $db->query("SELECT id,mail,pseudo,age,gender,isBanned FROM customer WHERE isBanned = 1");
@@ -13,34 +13,7 @@ $resultBannedDriver = $db->query("SELECT id,mail,pseudo,age,gender,isBanned FROM
 
 ?>
 
-<!DOCTYPE html>
-<html>
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
-  <link rel="stylesheet" href="css/theme.css" type="text/css">
-</head>
-
-<body class="bg-light h-100 w-100">
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <div class="container"> <a class="navbar-brand" href="#">
-        <i class="fa d-inline fa-lg fa-stop-circle"></i>
-        <b> Ride'N</b>
-      </a> <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar10">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbar10">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item"> <a class="nav-link" href="#">Features</a> </li>
-          <li class="nav-item"> <a class="nav-link" href="#">Pricing</a> </li>
-          <li class="nav-item"> <a class="nav-link" href="#">About</a> </li>
-          <li class="nav-item"> <a class="nav-link" href="#">FAQ</a> </li>
-        </ul> <a class="btn navbar-btn ml-md-2 btn-light text-dark">Contact us</a>
-      </div>
-    </div>
-  </nav>
   <div class="py-5">
   <div class="container">
     <div class="row">
@@ -69,7 +42,7 @@ $resultBannedDriver = $db->query("SELECT id,mail,pseudo,age,gender,isBanned FROM
                         </thead>
                         <tbody>
                           <?php $manage = new Manage($resultUnbanned);
-                                $manage->showUser($resultUnbanned); 
+                                $manage->showUser($resultUnbanned);
                          ?>
 
                         </tbody>
